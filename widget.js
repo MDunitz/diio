a9f74ae8524f16053f532813482a135724555bc9
Madison <midunitz@ucdavis.edu>
2016-07-23 18:02:43 -0700

    working modal box

--- widget.js
+++ widget.js
@@ -1,5 +1,6 @@
-function dialogBoxBasic(msg, title, onClick){
-  var msgBoxDivStyle = "background: rgba(127, 127, 127, 0.6);height: 100%;position: absolute;width: 100%;z-index: 9999;";
+function dialogBoxBasic(msg, title, onOk){
+  try {
+    var msgBoxDivStyle = "background: rgba(127, 127, 127, 0.6);height: 100%;position: absolute;width: 100%;z-index: 9999;";
     var msgBoxTitleStyle = "background-color: #00478A;color: white;font-size: 20px;font-weight: bold;left: 0px;padding: 4px;text-align: left;top: 0px;";
     var msgBoxErrorTitleStyle = "background-color: red;color: white;font-size: 20px;font-weight: bold;left: 0px;padding: 4px;text-align: left;top: 0px;";
     //var msgBoxSuccessTitleStyle = "background-color: green;color: white;font-size: 20px;font-weight: bold;left: 0px;padding: 4px;text-align: left;top: 0px;";
@@ -13,12 +14,30 @@ function dialogBoxBasic(msg, title, onClick){
     if(!title){
       title = "Hello";
     }
-    if(!onClick){
-      onClick="";
-    }
+    // if(onOk == null){
+    //   onOk="";
+    // }
     var titleStyle = msgBoxTitleStyle;
     if(title === "Error"){
       titleStyle=msgBoxErrorTitleStyle;
     }
+    msgDiv += "<div id='msgboxDiv' style='" + msgBoxDivStyle + "'>";  // Covers the entire body to prevent any interaction with controls in the body.
+    msgDiv += "<div id='msgboxContents' style='" + msgBoxContentsStyle + "'>";  // Contains the messagebox itself
+    msgDiv += "<div id='msgboxTitle' style='" + titleStyle + "'>" + title + "</div>";
+    msgDiv += "<div id='msgboxText' style='" + msgBoxTextStyle + "'>" + msg + "</div>";  // Contains the text of the message to be displayed.
+    msgDiv += "<button id='answerOK' tabindex='1' style='" + msgBoxButtonStyle + "' accesskey='K' onclick='document.body.removeChild(this.parentElement.parentElement);" + "'>O<u>K</u></button>"
+    msgDiv += "</div></div>";  // Closes the modalBox and modalDiv tags
 
-}
+    console.log('msgDiv', msgDiv)
+    var d1 = document.getElementById('one')
+
+    document.body.insertAdjacentHTML("beforebegin", msgDiv);
+    document.body.focus();
+
+  } catch(e){
+    console.log(e);
+  }
+
+  console.log(msg, title)
+};
+
