347db7bd593c8c82f100317f22c72d6778fb9a3f
Madison <midunitz@ucdavis.edu>
2016-07-24 20:16:05 -0700

    WIP

--- widget.js
+++ widget.js
@@ -14,7 +14,12 @@ function dialogBoxBasic(msg, title, type){
       error: 'msgBoxErrorTitle'
     }
  
-    var msgDiv = "<div class='msgBoxDiv'><div class='msgBoxContents'><div class="+ messageTypes[type] + ">" + title + "</div><div class='msgBoxText'>" + msg + "</div><button class='answerOK' tabindex='1' accesskey='K' onclick='document.body.removeChild(this.parentElement.parentElement);" + "'>O<u>K</u></button></div></div>";  // Closes the modalBox and modalDiv tags
+    var msgDiv = "<div class='msgBoxDiv' id='msgBox'><div class='msgBoxContents'><div class="+ 
+    messageTypes[type] + ">" + 
+    title + "</div><div class='msgBoxText'>" + 
+    msg + 
+    "</div><button class='answerOK' tabindex='1' accesskey='K' onkeydown='on_esc()' onclick='removeModal.bind(this)()" + 
+    "'>O<u>K</u></button></div></div>";
 
     console.log('msgDiv', msgDiv)
     var d1 = document.getElementById('one')
@@ -28,4 +33,16 @@ function dialogBoxBasic(msg, title, type){
 
   console.log(msg, title)
 };
+
+function removeModal(){
+  console.log('in removeModal', this)
+  document.body.removeChild(this.parentElement.parentElement);
+}
+
+function on_esc(){
+  console.log('key pressed?')
+  if(event.keyCode===27){
+    console.log('you hit the esc button')
+  }
+}
 
