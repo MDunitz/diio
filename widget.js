cdc5e6470c8f46df76a71a16f84e35c26a543748
Madison <midunitz@ucdavis.edu>
2016-07-25 20:08:36 -0700

    (WIP) close on esc

--- widget.js
+++ widget.js
@@ -1,37 +1,44 @@
-function dialogBoxBasic(msg, title, type){
+function dialogBoxBasic(msg, type, title){
   try {
-    if(!title){
-      title = "Hello";
+    var messageTypes = {
+      basic: ['msgBoxTitle', 'Basic'],
+      success: ['msgBoxSuccessTitle', 'Success'],
+      warning: ['msgBoxWarningTitle', 'Warning'],
+      error: ['msgBoxErrorTitle', 'Error']
     }
+
     if(!type){
       type = 'basic';
     }
-
-    var messageTypes = {
-      basic: 'msgBoxTitle',
-      success: 'msgBoxSuccessTitle',
-      warning: 'msgBoxWarningTitle',
-      error: 'msgBoxErrorTitle'
+    if(title===undefined){
+      title = messageTypes[type][1];
     }
- 
+    
     var msgDiv = "<div class='msgBoxDiv' id='msgBox'><div class='msgBoxContents'><div class="+ 
-    messageTypes[type] + ">" + 
-    title + "</div><div class='msgBoxText'>" + 
-    msg + 
-    "</div><button class='answerOK' tabindex='1' accesskey='K' onkeydown='on_esc()' onclick='removeModal.bind(this)()" + 
-    "'>O<u>K</u></button></div></div>";
+      messageTypes[type][0] + ">" + 
+      title + "</div><div class='msgBoxText'>" + 
+      msg + 
+      "</div><button class='answerOK' tabindex='1' accesskey='K' onkeydown='on_esc()' onclick='removeModal.bind(this)()" + 
+      "'>O<u>K</u></button></div></div>";
 
-    console.log('msgDiv', msgDiv)
     var d1 = document.getElementById('one')
-
     document.body.insertAdjacentHTML("beforeend", msgDiv);
     document.body.focus();
+    var msg=document.getElementById('msgBox');
+    console.log('msg', msg)
+    msg.addEventListener("keydown", onEsc())
+    
 
   } catch(e){
     console.log(e);
   }
+};
 
-  console.log(msg, title)
+function onEsc(){
+  console.log('key pressed?', event)
+  if(event.keyCode===27){
+    console.log('you hit the esc button')
+  }   
 };
 
 function removeModal(){
@@ -39,10 +46,5 @@ function removeModal(){
   document.body.removeChild(this.parentElement.parentElement);
 }
 
-function on_esc(){
-  console.log('key pressed?')
-  if(event.keyCode===27){
-    console.log('you hit the esc button')
-  }
-}
+
 
