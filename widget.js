988f24856cd6f66709fc26739b5d75a6d2c5d285
Madison <midunitz@ucdavis.edu>
2016-07-26 21:29:24 -0700

    (WIP) close on esc complete, different style popup options for user

--- widget.js
+++ widget.js
@@ -14,31 +14,29 @@ function dialogBoxBasic(msg, type, title){
       title = messageTypes[type][1];
     }
     
-    var msgDiv = "<div class='msgBoxDiv' id='msgBox'><div class='msgBoxContents'><div class="+ 
+    var msgDiv = "<div class='msgBoxDiv'><div class='msgBoxContents'><div class="+ 
       messageTypes[type][0] + ">" + 
       title + "</div><div class='msgBoxText'>" + 
       msg + 
-      "</div><button class='answerOK' tabindex='1' accesskey='K' onkeydown='on_esc()' onclick='removeModal.bind(this)()" + 
+      "</div><button class='answerOK' id='okButton' tabindex='1' onclick='removeModal.bind(this)()" + 
       "'>O<u>K</u></button></div></div>";
 
     var d1 = document.getElementById('one')
     document.body.insertAdjacentHTML("beforeend", msgDiv);
     document.body.focus();
-    var msg=document.getElementById('msgBox');
-    console.log('msg', msg)
-    msg.addEventListener("keydown", onEsc())
     
+
+    document.getElementById('okButton').onkeydown = function(button){
+      if(button.keyCode===27){
+        removeModal.bind(this)();
+        return false;
+      }
+    }
+    document.getElementById('okButton').focus();
 
   } catch(e){
     console.log(e);
   }
-};
-
-function onEsc(){
-  console.log('key pressed?', event)
-  if(event.keyCode===27){
-    console.log('you hit the esc button')
-  }   
 };
 
 function removeModal(){
